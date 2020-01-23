61ca865b011bda2e65bf77dfd65621743a7ef60b
Berava <berava@gmail.com>
2020-01-23 22:34:20 +0100

    Add stop animation during window resizing - js

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,3 +17,13 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('toggle');
 });
 
+// Stop Animations During Window Resizing
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
+
